28b601ac4ae25d62fe0934a7773641083388dcf4
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2023-08-10 16:49:03 +0200

    Update JavaScriptTesting

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.getElementById("myButton").addEventListener("click", function() {
-    document.getElementById("demo").innerHTML = "Hello, JavaScript is working!";
+document.addEventListener("DOMContentLoaded", function() {
+    const countElement = document.getElementById("count");
+    const incrementButton = document.getElementById("incrementButton");
+
+    // Retrieve the count from localStorage if available
+    let count = parseInt(localStorage.getItem("count")) || 0;
+    countElement.textContent = count;
+
+    // Update the count on button click
+    incrementButton.addEventListener("click", function() {
+        count++;
+        countElement.textContent = count;
+
+        // Save the updated count to localStorage
+        localStorage.setItem("count", count);
+    });
 });
