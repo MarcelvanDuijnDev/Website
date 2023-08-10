fc6dda9073d6adcd31983a99b02872ddd981be4c
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2023-08-10 17:14:05 +0200

    Update script.js > added test game

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const countElement = document.getElementById("count");
-    const incrementButton = document.getElementById("incrementButton");
+    let money = parseFloat(localStorage.getItem("money")) || 0;
+    let moneyPerSecond = parseFloat(localStorage.getItem("moneyPerSecond")) || 0;
+    let workers = parseInt(localStorage.getItem("workers")) || 0;
 
-    // Retrieve the count from localStorage if available
-    let count = parseInt(localStorage.getItem("count")) || 0;
-    countElement.textContent = count;
+    const moneyElement = document.getElementById("money");
+    const moneyPerSecondElement = document.getElementById("moneyPerSecond");
+    const earnMoneyButton = document.getElementById("earnMoneyButton");
+    const buyWorkerButton = document.getElementById("buyWorkerButton");
 
-    // Update the count on button click
-    incrementButton.addEventListener("click", function() {
-        count++;
-        countElement.textContent = count;
+    function saveGame() {
+        localStorage.setItem("money", money);
+        localStorage.setItem("moneyPerSecond", moneyPerSecond);
+        localStorage.setItem("workers", workers);
+    }
 
-        // Save the updated count to localStorage
-        localStorage.setItem("count", count);
+    function loadGame() {
+        moneyElement.textContent = money.toFixed(2);
+        moneyPerSecondElement.textContent = moneyPerSecond.toFixed(2);
+    }
+
+    earnMoneyButton.addEventListener("click", function() {
+        money += 1;
+        updateMoneyDisplay();
+        saveGame();
     });
+
+    buyWorkerButton.addEventListener("click", function() {
+        if (money >= 10) {
+            money -= 10;
+            workers += 1;
+            moneyPerSecond += 1;
+            updateMoneyDisplay();
+            updateMoneyPerSecondDisplay();
+            saveGame();
+        } else {
+            alert("Not enough money to buy a worker!");
+        }
+    });
+
+    function updateMoneyDisplay() {
+        moneyElement.textContent = money.toFixed(2);
+    }
+
+    function updateMoneyPerSecondDisplay() {
+        moneyPerSecondElement.textContent = moneyPerSecond.toFixed(2);
+    }
+
+    function updateMoneyPerSecond() {
+        money += workers;
+        updateMoneyDisplay();
+        saveGame();
+        setTimeout(updateMoneyPerSecond, 1000);
+    }
+
+    // Save the game when the user leaves the page
+    window.addEventListener("beforeunload", function() {
+        saveGame();
+    });
+
+    // Load the game on page load
+    loadGame();
+
+    // Start the game loop
+    updateMoneyPerSecond();
 });
